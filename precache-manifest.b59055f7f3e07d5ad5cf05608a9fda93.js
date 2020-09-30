e06a2289297f6ab3deeea90ed486bdc38f723231
Ernest <e.perkowski@student.utwente.nl>
2020-09-30 16:52:15 +0200

    Updates

---
+++ precache-manifest.b59055f7f3e07d5ad5cf05608a9fda93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5e209f070db74d0c45cdf195928461",
+    "url": "/velox/index.html"
+  },
+  {
+    "revision": "02e745ab066e81ab5bc0",
+    "url": "/velox/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "04e94d661db27013a8e5",
+    "url": "/velox/static/css/main.926cddff.chunk.css"
+  },
+  {
+    "revision": "02e745ab066e81ab5bc0",
+    "url": "/velox/static/js/2.608306ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/velox/static/js/2.608306ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e94d661db27013a8e5",
+    "url": "/velox/static/js/main.9e4270ec.chunk.js"
+  },
+  {
+    "revision": "95f391958c31f26cb46e",
+    "url": "/velox/static/js/runtime-main.336f0755.js"
+  }
+]);
