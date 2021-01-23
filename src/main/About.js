a24a47d94173033e23516227b0a37fe01ad3673c
Ernest Perkowski <perkowski.ernest98@gmail.com>
2021-01-23 09:38:31 +0100

    copyright, some styling, minor improvements

--- src/main/About.js
+++ src/main/About.js
@@ -13,6 +13,23 @@ class About extends Component {
         return (
             <div className="main-container">
                 <div className="coming-soon">Coming soon!</div>
+
+
+                {/* <span>Additional Info</span>
+                        <p>
+                        Velox skorengöring är ett högeffektivt rengöringsmedel.
+                        <br/>
+                        Primärt är produkten tänkt att användas på vardagliga skor men fungerar på krosstövlar, ridstövlar, fotbollsskor samt andra föremål av textilliknande material.
+                        <br/>
+                        <br/>
+                        Produkten är avsedd för att användas på alla typer av skor inklusive skinn, läder och mocka
+                        <br/>
+                        <br/>
+                        Vår högst moderna formula innehåller också en fantastisk odöreliminerande funktion som blockerar och eliminerar illaluktande doft i skorna.
+                        <br/><br/>
+
+                        Med doft av en fräsch melon kommer Velox inte bara få dina skor att skina utan även lukta gott.
+</p> */}
             </div>
         )
     }
